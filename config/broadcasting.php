ecc2def4cd27641d951ed23e5aded94abd3f8765
69pRomens <roman.kolosov12@yandex.ru>
2019-05-18 15:01:44 +0300

    try change broadcast to https

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => env('APP_URL'),
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'https',
             ],
