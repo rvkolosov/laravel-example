9c3f3526338213872b45898fff4a718de1ebab08
69pRomens <roman.kolosov12@yandex.ru>
2019-05-18 18:04:06 +0300

    try change broadcast to https

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,6 +40,7 @@ return [
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'https',
+                'encrypted' => 'true',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
