bef902c5df8d05366933f84e9014547f0630344e
69pRomens <roman.kolosov12@yandex.ru>
2019-05-18 18:32:45 +0300

    try change broadcast to https

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,8 +39,8 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'https',
-                'encrypted' => 'true',
+                'scheme' => 'http',
+                //'encrypted' => 'true',
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
