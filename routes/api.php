1dbbd021ae988989847dcfc9a165c33a2ebb08a8
69pRomens <roman.kolosov12@yandex.ru>
2019-06-20 01:37:32 +0300

    delete Elastiquent

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::post('login', function (Request $request) {
+Route::post('login', function () {
     redirect('login');
 });
 
