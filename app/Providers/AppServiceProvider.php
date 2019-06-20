46fb03802bfa13a3400415172569b703fd33b57a
69pRomens <roman.kolosov12@yandex.ru>
2019-06-20 15:48:18 +0300

    change api auth to passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Observers\UserObserver;
+use App\User;
 use Redis;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        User::observe(UserObserver::class);
     }
 }
