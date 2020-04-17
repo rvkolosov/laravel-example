f3436022d45c45585eb8672c655746b65965aed4
Roman V. Kolosov <roman.kolosov12@yandex.ru>
2020-04-17 23:42:49 +0300

    Fix channels

--- config/app.php
+++ config/app.php
@@ -179,7 +179,7 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
-        //App\Providers\TelescopeServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
